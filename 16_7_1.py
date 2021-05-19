a05a244e0e542428218ecde0b554ed3010f0d7ed
ALEKSEI RUKINOV <58972919+Alruk-art@users.noreply.github.com>
2021-05-19 21:06:46 +0300

    Кошки в приюте

---
+++ 16_7_1.py
@@ -0,0 +1,6 @@
+from PetCat import Cat
+print('Кошки в приюте')
+pet1 = Cat("Сэм", "мальчик", 3)
+print (f'Имя: {pet1.name}, пол: {pet1.gender}, возраст: {pet1.age}')
+pet2 = Cat("Барон", "кот", 2)
+print (f'Имя: {pet2.name}, пол: {pet2.gender}, возраст: {pet2.age}')
